58f668a305d663c2a3c449f5de24c515507479b1
김지환 <jihwan-morning@mz.co.kr>
2023-08-16 16:12:42 +0900

    feat: 환경설정 및 redux 폴더구조 세팅
    환경설정과 redux 폴더구조등 세팅 반영

--- App.tsx
+++ App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { HomeScreen } from "./src/screens/HomeScreen";
+import { LandingScreen } from "./src/screens/LandingScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+  return <LandingScreen />;
 }
 
 const styles = StyleSheet.create({
